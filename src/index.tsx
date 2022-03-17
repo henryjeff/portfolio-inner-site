ba570ef37bbec60d007e581acf0af17fd9415f41
henryjeff <henryheffernan@gmail.com>
2022-03-17 10:51:30 -0400

    moved again

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+window.parent.postMessage('hello from the iframe', '*');
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
