cb5f6605a01c091248e47d6aba1a80f58def6282
henryjeff <henryheffernan@gmail.com>
2022-03-16 23:20:15 -0400

    cross origin message setup

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,21 @@ import './App.css';
 import Desktop from './components/os/Desktop';
 
 function App() {
+    window.addEventListener('message', (event) => {
+        // IMPORTANT: check the origin of the data!
+        console.log(event.origin);
+        // if (event.origin.startsWith('http://your-first-site.com')) {
+        //     // The data was sent from your site.
+        //     // Data sent with postMessage is stored in event.data:
+        //     console.log(event.data);
+        // } else {
+        //     // The data was NOT sent from your site!
+        //     // Be careful! Do not use it. This else branch is
+        //     // here just for clarity, you usually shouldn't need it.
+        //     return;
+        // }
+    });
+
     return (
         <div className="App">
             <Desktop />
