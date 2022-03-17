6297a4599f2b2daec72ed9921ea70ff9007e9175
henryjeff <henryheffernan@gmail.com>
2022-03-16 23:33:32 -0400

    update url

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,10 @@ function App() {
     //     '_blank'
     // );
 
-    window.postMessage('Hello from the other side!', '*');
+    window.postMessage(
+        'Hello from the other side!',
+        'http://192.168.0.19:8080/'
+    );
     // site.sendMessage('Hello from the app!', 'http://localhost:8080/');
 
     return (
