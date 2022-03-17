457562dbedbe8225ed698185d6cfc03672491b1e
henryjeff <henryheffernan@gmail.com>
2022-03-16 23:29:58 -0400

    post message test

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,27 @@ import './App.css';
 import Desktop from './components/os/Desktop';
 
 function App() {
-    window.addEventListener('message', (event) => {
-        // IMPORTANT: check the origin of the data!
-        console.log(event.origin);
-        // if (event.origin.startsWith('http://your-first-site.com')) {
-        //     // The data was sent from your site.
-        //     // Data sent with postMessage is stored in event.data:
-        //     console.log(event.data);
-        // } else {
-        //     // The data was NOT sent from your site!
-        //     // Be careful! Do not use it. This else branch is
-        //     // here just for clarity, you usually shouldn't need it.
-        //     return;
-        // }
-    });
+    // window.addEventListener('message', (event) => {
+    // IMPORTANT: check the origin of the data!
+    // console.log(event.origin);
+    // if (event.origin.startsWith('http://your-first-site.com')) {
+    //     // The data was sent from your site.
+    //     // Data sent with postMessage is stored in event.data:
+    //     console.log(event.data);
+    // } else {
+    //     // The data was NOT sent from your site!
+    //     // Be careful! Do not use it. This else branch is
+    //     // here just for clarity, you usually shouldn't need it.
+    //     return;
+    // }
+    // });
+    // const site = window.open(
+    //     'http://localhost:http://192.168.0.19:8080/',
+    //     '_blank'
+    // );
+
+    window.postMessage('Hello from the other side!', '*');
+    // site.sendMessage('Hello from the app!', 'http://localhost:8080/');
 
     return (
         <div className="App">
