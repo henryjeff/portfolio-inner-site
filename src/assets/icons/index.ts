ce9729a2d833cd67d0f9a4ff8b8b25308fadfe58
henryjeff <henryheffernan@gmail.com>
2022-04-15 17:54:25 -0400

    new assets

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,13 +3,25 @@ import React from 'react';
 import windowResize from './windowResize.png';
 import maximize from './maximize.png';
 import minimize from './minimize.png';
+import computerBig from './computerBig.png';
+import myComputer from './myComputer.png';
+import showcaseIcon from './showcaseIcon.png';
+import doomIcon from './doomIcon.png';
+import trailIcon from './trailIcon.png';
+import windowGameIcon from './windowGameIcon.png';
 import close from './close.png';
 
 const icons = {
     windowResize: windowResize,
     maximize: maximize,
     minimize: minimize,
+    computerBig: computerBig,
+    myComputer: myComputer,
+    showcaseIcon: showcaseIcon,
+    doomIcon: doomIcon,
     close: close,
+    windowGameIcon: windowGameIcon,
+    trailIcon: trailIcon,
 };
 
 export type IconName = keyof typeof icons;
