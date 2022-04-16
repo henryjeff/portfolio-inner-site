36c6a83d8a1dfcb37d6cb948b8e1781992ae8152
henryjeff <henryheffernan@gmail.com>
2022-04-15 21:06:48 -0400

    added scrabble

---
+++ src/components/applications/Scrabble.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import DosPlayer from '../dos/DosPlayer';
+import Window from '../os/Window';
+
+export interface ScrabbleAppProps extends WindowAppProps {}
+
+const ScrabbleApp: React.FC<ScrabbleAppProps> = (props) => {
+    const [width, setWidth] = useState(920);
+    const [height, setHeight] = useState(750);
+
+    return (
+        <Window
+            top={10}
+            left={10}
+            width={width}
+            height={height}
+            windowTitle="Scrabble"
+            windowBarIcon="windowGameIcon"
+            windowBarColor="#941d13"
+            closeWindow={props.onClose}
+            onInteract={props.onInteract}
+            onWidthChange={setWidth}
+            onHeightChange={setHeight}
+        >
+            <DosPlayer
+                width={width}
+                height={height}
+                bundleUrl="scrabble.jsdos"
+            />
+        </Window>
+    );
+};
+
+export default ScrabbleApp;
