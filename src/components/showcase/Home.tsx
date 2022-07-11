b31855c1d680e7e5b1e9ddaee461d18d2b90f70b
henryjeff <henryheffernan@gmail.com>
2022-07-11 14:00:57 -0400

    hired v2

--- src/components/showcase/Home.tsx
+++ src/components/showcase/Home.tsx
@@ -38,7 +38,7 @@ const Home: React.FC<HomeProps> = (props) => {
                 />
             </div>
             <div style={styles.forHireContainer} onMouseDown={goToContact}>
-                <img src={forhire} alt="" />
+                {/* <img src={forhire} alt="" /> */}
             </div>
         </div>
     );
