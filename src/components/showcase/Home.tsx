230be5126bd8966647be3fc301c68101cf21b1b1
Luca Dante <lucadante201005@yahoo.com>
2025-12-17 20:24:29 +0100

    Update name from Henry Heffernan to Luca Dante

--- src/components/showcase/Home.tsx
+++ src/components/showcase/Home.tsx
@@ -15,7 +15,7 @@ const Home: React.FC<HomeProps> = (props) => {
     return (
         <div style={styles.page}>
             <div style={styles.header}>
-                <h1 style={styles.name}>Henry Heffernan</h1>
+                <h1 style={styles.name}>Luca Dante</h1>
                 <h2>Software Engineer</h2>
             </div>
             <div style={styles.buttons}>
