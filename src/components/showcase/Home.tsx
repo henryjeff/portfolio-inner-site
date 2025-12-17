c1d66722776618e53963ebb08939472141d5bdd6
Luca Dante <lucadante201005@yahoo.com>
2025-12-17 20:26:31 +0100

    Update Home.tsx

--- src/components/showcase/Home.tsx
+++ src/components/showcase/Home.tsx
@@ -16,7 +16,7 @@ const Home: React.FC<HomeProps> = (props) => {
         <div style={styles.page}>
             <div style={styles.header}>
                 <h1 style={styles.name}>Luca Dante</h1>
-                <h2>Software Engineer</h2>
+                <h2>aspirante graphic designer</h2>
             </div>
             <div style={styles.buttons}>
                 <Link containerStyle={styles.link} to="about" text="ABOUT" />
