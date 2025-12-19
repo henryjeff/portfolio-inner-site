3a0e516902b6761fe3fc65ed1727a6a414056d96
Luca Dante <lucadante201005@yahoo.com>
2025-12-19 22:04:04 +0100

    Update ShowcaseExplorer.tsx

--- src/components/applications/ShowcaseExplorer.tsx
+++ src/components/applications/ShowcaseExplorer.tsx
@@ -23,12 +23,12 @@ const ShowcaseExplorer: React.FC<ShowcaseExplorerProps> = (props) => {
             left={56}
             width={initWidth}
             height={initHeight}
-            windowTitle="Henry Heffernan - Showcase 2022"
+            windowTitle="chiomao.in"
             windowBarIcon="windowExplorerIcon"
             closeWindow={props.onClose}
             onInteract={props.onInteract}
             minimizeWindow={props.onMinimize}
-            bottomLeftText={'© Copyright 2022 Henry Heffernan'}
+            bottomLeftText={'© Copyright 2025 Luca Dante'}
         >
             <Router>
                 <div className="site-page">
