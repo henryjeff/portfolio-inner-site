a761de1aead75b52fcceb5b284d10756f9564396
Luca Dante <lucadante201005@yahoo.com>
2025-12-19 20:44:19 +0100

    Update Desktop.tsx

--- src/components/os/Desktop.tsx
+++ src/components/os/Desktop.tsx
@@ -105,7 +105,7 @@ const Desktop: React.FC<DesktopProps> = (props) => {
         });
 
         newShortcuts.forEach((shortcut) => {
-            if (shortcut.shortcutName === 'My Showcase') {
+            if (shortcut.shortcutName === 'Portfolio') {
                 shortcut.onOpen();
             }
         });
